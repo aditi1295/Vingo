f1463cb24b25a2175df593afe93a941ca19fdc91
12aditi95 <aditi1295porwal@gmail.com>
2026-04-12 23:19:10 +0530

    implemented authentication middleware and creating and fecth getcurrent user api

---
+++ frontend/hooks/useGetCurrentUser.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { serverUrl } from '../src/App';
+import { useEffect } from 'react';
+import axios from 'axios';
+
+
+function useGetCurrentUser() {
+    useEffect(() => {
+  const fetchUser=async()=>{
+
+    try {
+        const result=await axios.get(`${serverUrl}/api/user/current`,{withCredentials:true});
+         console.log(result);
+    } catch (error) {
+   console.log(error);
+        
+    }
+  }
+  fetchUser();
+
+},[])
+}
+
+export default useGetCurrentUser;
